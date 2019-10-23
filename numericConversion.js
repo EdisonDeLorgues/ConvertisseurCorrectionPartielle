541303f24ddead91f88c8fd6374859d3ff9611ca
EdisonDeLorgues <54467505+EdisonDeLorgues@users.noreply.github.com>
2019-10-23 13:06:29 +0200

    Add files via upload

---
+++ numericConversion.js
@@ -0,0 +1,40 @@
+function hexWordToBin(hexWord){
+	//Ecrire une fonction de conversion d'un mot hexadécimal vers le mot binaire
+	// correspondant
+
+	let binWord = "";
+	//Mettre à jour binWord à l'aide d'une boucle
+	return binWord;
+}
+
+function hexDigitToBin(hexDigit){
+	//Compléter cette fonction de conversion d'un caaractère représentant un chiffre hexadécimal
+	//retournant le mot binaire correspondant.
+	upperCaseHexDigit = hexDigit.toUpperCase()
+	return ;
+}
+
+function hexToBinMapping(){
+	//Compléter cette fonction construisant un objet de mapping
+	//qui fait correspondre aux chiffres hexadécimaus les mots
+	//binaires correspondants.
+	const result = {
+	"0":"",
+	"1":"",
+	"2":"",
+	"3":"",
+	"4":"",
+	"5":"",
+	"6":"",
+	"7":"",
+	"8":"",
+	"9":"",
+	"A":"",
+	"B":"",
+	"C":"",
+	"D":"",
+	"E":"",
+	"F":""
+	}
+	return
+}
